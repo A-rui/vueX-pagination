9e5ea131e40843598c462b1e6aecaed7af6e8044
A-rui <rui-com@qq.com>
2019-07-26 15:22:45 +0800

    封装的组件

---
+++ src/lib/Pagination.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="pagination">
+      <!-- <ul v-if="currentPage"> -->
+      display:{{display}}
+      <!-- </ul> -->
+    </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+      }
+    },
+    props: {
+      display: {// 每页显示条数
+        type: Number
+      }
+    },
+    computed: {
+    },
+    methods: {
+      inationAdd(index) {
+        this.$emit('pagechange', index);
+      }
+    },
+    mounted() {
+    },
+  }
+</script>
+<style>
+
+</style>
